174f767c3beac95a0ba631e3ce59df0f23bf17ae
Sandeep jangra <sandeepjangra1011@gmail.com>
2024-12-09 16:35:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+#Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7582312095:AAExNX9Nqq4_ufZh4XW06MDDNlnNpW87CSg")
+
+#Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "26704962"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "6c0013f5b2e558b681a4967c2d5f5047")
+
+#Database 
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://molasa7348:29TiLiVemOcKUmxf@cluster0.9dkky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
